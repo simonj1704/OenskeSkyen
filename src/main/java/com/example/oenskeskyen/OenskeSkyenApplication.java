487ac2e8b332697e9090b9e30e4f3a906a89971b
simonj1704 <simo915m@stud.kea.dk>
2023-03-30 15:06:08 +0200

    First Commit of Project

---
+++ src/main/java/com/example/oenskeskyen/OenskeSkyenApplication.java
@@ -0,0 +1,13 @@
+package com.example.oenskeskyen;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OenskeSkyenApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OenskeSkyenApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oenskeskyen/OenskeSkyenApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oenskeskyen;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OenskeSkyenApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
