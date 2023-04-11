3a19a280855df2e8d8cb15f5f64a4a4022f60ee4
simonj1704 <113358970+simonj1704@users.noreply.github.com>
2023-04-11 12:43:41 +0200

    Add files via upload

--- Create oenskeskyen DB.sql
+++ Create oenskeskyen DB.sql
@@ -2,5 +2,46 @@ DROP DATABASE IF EXISTS oenskeskyen;
 CREATE DATABASE oenskeskyen;
 USE oenskeskyen;
 
+CREATE TABLE users
+(
+ user_id 	int 	NOT NULL 	PRIMARY KEY 	AUTO_INCREMENT,
+ username 	VARCHAR(45) NOT NULL,
+ passcode VARCHAR(45) NOT NULL,
+ first_name VARCHAR(45),
+ last_name VARCHAR(45),
+ email VARCHAR(45),
+ phone_number VARCHAR(45)
+);
+
+CREATE TABLE wish
+(
+wish_id 		INT		NOT NULL 	PRIMARY KEY		AUTO_INCREMENT,
+wish_name VARCHAR(45),
+wish_link VARCHAR(45),
+reserved BOOLEAN
+);
+
+
+CREATE TABLE wishlist
+(
+wishlist_id		INT		NOT NULL 	PRIMARY KEY 	AUTO_INCREMENT,
+user_id INT,
+wishlist_name VARCHAR(45),
+wish_id	INT,
+FOREIGN KEY (wish_id) REFERENCES wish(wish_id),
+FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+
+
+INSERT INTO users (first_name, last_name, email, phone_number, username, passcode) VALUES
+("Test", "User", "testUser@gmail.com", "123456", "test Username", "password"),
+("test2", "user2", "testUser2@gmail.com", "654321", "username", "hej");
+
+INSERT INTO wish (wish_name, wish_link, reserved) VALUES
+("Wish#1", "WishLink", false);
+
+INSERT INTO wishlist (wishlist_name, user_id, wish_id) VALUES 
+("wishllist name", 1, 1);
 
 
