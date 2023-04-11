7de0758778b96028442c1c8ffcdddd660e2b9d40
simonj1704 <113358970+simonj1704@users.noreply.github.com>
2023-04-11 14:39:39 +0200

    Add files via upload

--- Create oenskeskyen DB.sql
+++ Create oenskeskyen DB.sql
@@ -9,39 +9,42 @@ CREATE TABLE users
  passcode VARCHAR(45) NOT NULL,
  first_name VARCHAR(45),
  last_name VARCHAR(45),
- email VARCHAR(45),
- phone_number VARCHAR(45)
+ email VARCHAR(45)
 );
-
-CREATE TABLE wish
-(
-wish_id 		INT		NOT NULL 	PRIMARY KEY		AUTO_INCREMENT,
-wish_name VARCHAR(45),
-wish_link VARCHAR(45),
-reserved BOOLEAN
-);
-
 
 CREATE TABLE wishlist
 (
 wishlist_id		INT		NOT NULL 	PRIMARY KEY 	AUTO_INCREMENT,
 user_id INT,
 wishlist_name VARCHAR(45),
-wish_id	INT,
-FOREIGN KEY (wish_id) REFERENCES wish(wish_id),
 FOREIGN KEY (user_id) REFERENCES users(user_id)
 );
 
 
 
-INSERT INTO users (first_name, last_name, email, phone_number, username, passcode) VALUES
-("Test", "User", "testUser@gmail.com", "123456", "test Username", "password"),
-("test2", "user2", "testUser2@gmail.com", "654321", "username", "hej");
+CREATE TABLE wish
+(
+wish_id 		INT		NOT NULL 	PRIMARY KEY		AUTO_INCREMENT,
+wish_name VARCHAR(45),
+wish_link VARCHAR(45),
+wish_price DOUBLE,
+reserved BOOLEAN,
+wishlist_id INT,
+FOREIGN KEY (wishlist_id) REFERENCES wishlist(wishlist_id)
+);
 
-INSERT INTO wish (wish_name, wish_link, reserved) VALUES
-("Wish#1", "WishLink", false);
 
-INSERT INTO wishlist (wishlist_name, user_id, wish_id) VALUES 
-("wishllist name", 1, 1);
+
+INSERT INTO users (first_name, last_name, email, username, passcode) VALUES
+("Test", "User", "testUser@gmail.com", "test Username", "password"),
+("test2", "user2", "testUser2@gmail.com", "username", "hej");
+
+INSERT INTO wishlist (wishlist_name, user_id) VALUES 
+("wishlist name", 1);
+
+INSERT INTO wish (wish_name, wish_link, reserved, wish_price, wishlist_id) VALUES
+("Wish#1", "WishLink", false, 199.99, 1);
+
+
 
 
