6be0322b99b4556a1d0a9f7c85b055dcd6360935
Anders <113690119+acb100@users.noreply.github.com>
2023-04-14 10:26:56 +0200

    Made a page for viewing user data from top-right dropdown menu

--- src/main/java/com/example/oenskeskyen/controller/HomeController.java
+++ src/main/java/com/example/oenskeskyen/controller/HomeController.java
@@ -19,15 +19,15 @@ public class HomeController {
     WishService wishService;
 
     @GetMapping("/index")
-    public String index(){
+    public String index() {
         return "index";
     }
 
     @PostMapping("/index")
     public String userPage(@RequestParam("username") String username,
                            @RequestParam("password") String password,
-                           Model model, HttpSession session){
-        if (wishService.userExist(username, password)){
+                           Model model, HttpSession session) {
+        if (wishService.userExist(username, password)) {
             model.addAttribute("user", wishService.getUser(username));
             session.setAttribute("username", username);
             session.setAttribute("isLoggedIn", true);
@@ -39,7 +39,7 @@ public class HomeController {
     }
 
     @GetMapping("/userpage")
-    public String userpage(Model model, HttpServletRequest request){
+    public String userpage(Model model, HttpServletRequest request) {
         HttpSession session = request.getSession();
         Boolean isLoggedIn = (Boolean) session.getAttribute("isLoggedIn");
         if (isLoggedIn == null || !isLoggedIn) {
@@ -52,8 +52,16 @@ public class HomeController {
         }
     }
 
+    @GetMapping("/myprofile")
+    public String myProfile(HttpServletRequest request, Model model) {
+        HttpSession session = request.getSession();
+        String username = (String) session.getAttribute("username");
+        model.addAttribute("user", wishService.getUser(username));
+        return "myprofile";
+    }
+
     @GetMapping("/logout")
-    public String logout(HttpServletRequest request){
+    public String logout(HttpServletRequest request) {
         HttpSession session = request.getSession();
         session.setAttribute("isLoggedIn", false);
         session.invalidate();
@@ -61,29 +69,29 @@ public class HomeController {
     }
 
     @PostMapping("createUser")
-    public String createUser(@ModelAttribute User user){
+    public String createUser(@ModelAttribute User user) {
         wishService.addUser(user);
         return "redirect:/index";
     }
 
     @PostMapping("/createWishlist")
-    public String createWishlist(@ModelAttribute Wishlist wishlist, HttpServletRequest request){
+    public String createWishlist(@ModelAttribute Wishlist wishlist, HttpServletRequest request) {
         String username = (String) request.getSession().getAttribute("username");
-        wishService.addWishlist(wishlist,username);
+        wishService.addWishlist(wishlist, username);
         return "redirect:/userpage";
     }
 
     @GetMapping("/viewWishList/{wishlist_id}")
-    public String viewWishList(@PathVariable("wishlist_id") int wishlist_id, Model model){
+    public String viewWishList(@PathVariable("wishlist_id") int wishlist_id, Model model) {
         List<Wish> wishList = wishService.fetchAllWishes(wishlist_id);
         model.addAttribute("wishes", wishList);
         model.addAttribute("wishlist", wishlist_id);
         return "wishList";
     }
+
     @GetMapping("/addWish/{wishlist_id}")
-    public String addWish(@ModelAttribute Wish wish, @PathVariable("wishlist_id") int wishlist_id){
+    public String addWish(@ModelAttribute Wish wish, @PathVariable("wishlist_id") int wishlist_id) {
         wishService.addWishToWishlist(wish, wishlist_id);
         return "redirect:/viewWishList/" + wishlist_id;
     }
-
 }
