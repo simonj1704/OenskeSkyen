0725e18e9aa553c625466abc5b2d12e8978f366e
simonj1704 <simo915m@stud.kea.dk>
2023-04-17 09:56:42 +0200

    Created BAck button in wishlist.html

--- src/main/java/com/example/oenskeskyen/controller/HomeController.java
+++ src/main/java/com/example/oenskeskyen/controller/HomeController.java
@@ -12,6 +12,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+//todo delete wishlist
+//todo delete wish
 
 @Controller
 public class HomeController {
